c4fd4df386a2a73f979ca705d7f85e06be819919
Wilder Ap <wilderasprilla321@gmail.com>
2024-07-17 15:29:33 -0500

    laguage to spanish and social btns

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import Contact from './components/Contact/Contact';
-import Features from './components/Features/Features';
-import Footer from './components/Footer/Footer';
-import Hero from './components/Hero/Hero';
-import Info from './components/Info/Info';
-import Meet from './components/Meet/Meet';
-import Navbar from './components/Navbar/Navbar';
-import Projects from './components/Projects/Projects';
-import Reviews from './components/Reviews/Reviews';
-import Stats from './components/Stats/Stats';
-import ToolsYouNeed from './components/ToolsYouNeed/ToolsYouNeed';
-import Layout from './components/ui/Layout';
+import Contact from "./components/Contact/Contact";
+import Features from "./components/Features/Features";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Info from "./components/Info/Info";
+import Meet from "./components/Meet/Meet";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import Reviews from "./components/Reviews/Reviews";
+import ToolsYouNeed from "./components/ToolsYouNeed/ToolsYouNeed";
+import Layout from "./components/ui/Layout";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init({
   once: true,
@@ -22,7 +21,7 @@ function App() {
   return (
     <main>
       <Navbar />
-      <Layout maxWidth='max-w-[1440px]'>
+      <Layout maxWidth="max-w-[1440px]">
         <Hero />
         <ToolsYouNeed />
         <Info />
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,19 @@
-import Logo from '../ui/Logo';
-import RenderLinks from '../ui/RenderLinks';
+import SocialButtons from "../Socials";
+import Logo from "../ui/Logo";
+import RenderLinks from "../ui/RenderLinks";
 
 const Footer = () => {
   return (
-    <footer className='footer footer-center p-10 bg-custom-blue/gray-900 text-custom-white mt-20'>
+    <footer className="footer footer-center p-10 bg-custom-blue/gray-900 text-custom-white mt-20">
       <aside>
         <Logo inFooter />
-        <ul className='flex flex-wrap justify-center gap-4 my-4'>
+        <ul className="flex flex-wrap justify-center gap-4 my-4">
           {RenderLinks()}
         </ul>
-        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+        <SocialButtons />
+        <p>
+          Copyright © {new Date().getFullYear()} - Todos los derechos reservados
+        </p>
       </aside>
     </footer>
   );
